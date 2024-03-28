2ffa4d91cdf8a7c97ad69f1ef1b2b26d72724da8
Yingsiu Lau <yingsiu.lau@polypoint.ch>
2024-03-28 16:47:22 +0100

    added fade animation in contacts

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,3 +1,10 @@
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/service/data.service';
 
@@ -5,17 +12,25 @@ import { DataService } from 'src/app/service/data.service';
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
+  animations: [
+    trigger('visibleHidden', [
+      state(
+        'visible',
+        style({
+          opacity: 1,
+        })
+      ),
+      transition('* => visible', [animate('1s')]),
+    ]),
+  ],
 })
 export class ContactComponent implements OnInit {
   public password: string = '';
   public displayInfo: boolean = false;
   public insertedPassword = '';
   public displayWrong: boolean = false;
-  public pdfSource: string = 'assets/docs/2023_Yingsiu_Lebenslauf.pdf'
-
-  constructor(public dataService: DataService){
-
-  }
+  public pdfSource: string = 'assets/docs/2023_Yingsiu_Lebenslauf.pdf';
+  constructor(public dataService: DataService) {}
 
   ngOnInit(): void {
     this.generatePassword();
