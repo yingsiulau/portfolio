0530f1a13b34caeebdcc14a06ca2e24f10d7c761
Yingsiu Lau <yingsiulau@gmail.com>
2024-08-12 01:33:56 +0200

    added more albums

--- docs/assets/songlist.ts
+++ docs/assets/songlist.ts
@@ -1,10 +1,10 @@
-
 export const albumData: AlbumCollection = {
   albums: [
     {
       albumTitle: "Yellow and Such",
       artist: "Gus Dapperton",
       genre: "Indie Pop, Bedroom Pop",
+      releaseYear: 2017,
       songs: [
         {
           title: "Miss Glum & The Pursuit of Falling",
@@ -23,12 +23,13 @@ export const albumData: AlbumCollection = {
           length: "4:40"
         }
       ],
-      image_path: "assets/images/gus.jpg"
+      image_path: "assets/images/album/gus.jpg"
     },
     {
       albumTitle: "Charm",
       artist: "Clairo",
       genre: "Psychedelic Folk, Soul, Jazz",
+      releaseYear: 2024,
       songs: [
         {
           title: "Nomad",
@@ -75,12 +76,13 @@ export const albumData: AlbumCollection = {
           length: "3:25"
         }
       ],
-      image_path: "assets/images/clairo.png"
+      image_path: "assets/images/album/clairo.png"
     },
     {
       albumTitle: "Superclean Vol. II",
       artist: "The Marías",
       genre: "Dream Pop",
+      releaseYear: 2018,
       songs: [
         {
           title: "Ruthless",
@@ -103,7 +105,243 @@ export const albumData: AlbumCollection = {
           length: "3:28"
         }
       ],
-      image_path: "assets/images/superclean2.jpg"
+      image_path: "assets/images/album/superclean2.jpg"
+    },
+    {
+      albumTitle: "Another One",
+      artist: "Mac DeMarco",
+      genre: "Indie Rock",
+      releaseYear: 2015,
+      songs: [
+        {
+          title: "The Way You’d Love Her",
+          length: "2:37"
+        },
+        {
+          title: "Another One",
+          length: "2:40"
+        },
+        {
+          title: "No Other Heart",
+          length: "2:53"
+        },
+        {
+          title: "Just to Put Me Down",
+          length: "3:18"
+        },
+        {
+          title: "A Heart Like Hers",
+          length: "4:01"
+        },
+        {
+          title: "I've Been Waiting for Her",
+          length: "2:47"
+        },
+        {
+          title: "Without Me",
+          length: "2:59"
+        },
+        {
+          title: "My House by the Water",
+          length: "2:35"
+        }
+      ],
+      image_path: "assets/images/album/another_one.jpeg"
+    },
+    {
+      albumTitle: "Trench",
+      artist: "Twenty One Pilots",
+      genre: "Alternative Rock, Hip Hop, Electropop",
+      releaseYear: 2018,
+      songs: [
+        {
+          title: "Jumpsuit",
+          length: "3:58"
+        },
+        {
+          title: "Levitate",
+          length: "2:25"
+        },
+        {
+          title: "Morph",
+          length: "4:19"
+        },
+        {
+          title: "My Blood",
+          length: "3:49"
+        },
+        {
+          title: "Chlorine",
+          length: "5:24"
+        },
+        {
+          title: "Smithereens",
+          length: "2:57"
+        },
+        {
+          title: "Neon Gravestones",
+          length: "4:00"
+        },
+        {
+          title: "The Hype",
+          length: "4:25"
+        },
+        {
+          title: "Nico and the Niners",
+          length: "3:45"
+        },
+        {
+          title: "Cut My Lip",
+          length: "4:42"
+        },
+        {
+          title: "Bandito",
+          length: "5:30"
+        },
+        {
+          title: "Pet Cheetah",
+          length: "3:18"
+        },
+        {
+          title: "Legend",
+          length: "2:52"
+        },
+        {
+          title: "Leave the City",
+          length: "4:40"
+        }
+      ],
+      image_path: "assets/images/album/trench.jpeg"
+    },
+    {
+      albumTitle: "Orca",
+      artist: "Gus Dapperton",
+      genre: "Indie Pop, Alternative Rock",
+      releaseYear: 2020,
+      songs: [
+        {
+          title: "Bottle Opener",
+          length: "2:52"
+        },
+        {
+          title: "First Aid",
+          length: "3:15"
+        },
+        {
+          title: "Post Humorous",
+          length: "4:09"
+        },
+        {
+          title: "Bluebird",
+          length: "4:14"
+        },
+        {
+          title: "Palms",
+          length: "3:50"
+        },
+        {
+          title: "My Say So",
+          length: "3:56"
+        },
+        {
+          title: "Grim",
+          length: "4:12"
+        },
+        {
+          title: "Antidote",
+          length: "3:36"
+        },
+        {
+          title: "Medicine",
+          length: "3:37"
+        },
+        {
+          title: "Swan Song",
+          length: "4:12"
+        }
+      ],
+      image_path: "assets/images/album/orca.jpeg"
+    },
+    {
+      albumTitle: "Nectar",
+      artist: "Joji",
+      genre: "R&B, Pop, Alternative",
+      releaseYear: 2020,
+      songs: [
+        {
+          title: "Ew",
+          length: "3:25"
+        },
+        {
+          title: "MODUS",
+          length: "3:27"
+        },
+        {
+          title: "Tick Tock",
+          length: "2:13"
+        },
+        {
+          title: "Daylight",
+          length: "2:47"
+        },
+        {
+          title: "Upgrade",
+          length: "1:29"
+        },
+        {
+          title: "Gimme Love",
+          length: "3:34"
+        },
+        {
+          title: "Run",
+          length: "3:38"
+        },
+        {
+          title: "Sanctuary",
+          length: "3:00"
+        },
+        {
+          title: "High Hopes",
+          length: "3:17"
+        },
+        {
+          title: "NITROUS",
+          length: "2:13"
+        },
+        {
+          title: "Pretty Boy",
+          length: "3:37"
+        },
+        {
+          title: "Normal People",
+          length: "2:47"
+        },
+        {
+          title: "Afterthought",
+          length: "3:15"
+        },
+        {
+          title: "Mr. Hollywood",
+          length: "2:40"
+        },
+        {
+          title: "777",
+          length: "3:09"
+        },
+        {
+          title: "Reanimator",
+          length: "3:10"
+        },
+        {
+          title: "Like You Do",
+          length: "3:24"
+        },
+        {
+          title: "Your Man",
+          length: "3:14"
+        }
+      ],
+      image_path: "assets/images/album/nectar.jpeg"
     }
   ]
 };
